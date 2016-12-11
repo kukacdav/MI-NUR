73698044536045d5de80d0622154238ec825c116
David <david@kusco.cz>
2016-12-11 20:18:52 +0100

    Uprava MessageBoxu, pridani tlacitka zpet, pridani data a uzivatele k novym zpravam.

--- maps/messageBox/messageBox.js
+++ maps/messageBox/messageBox.js
@@ -6,7 +6,7 @@ loadMessages();
 function loadMessages () {
 	var HTML = [];
 	for (i = 0; i < 3; i++) { 
-		HTML.push( "<div class=" + messages[i].type + "><p>" + messages[i].content + "</p></div><br/><br/><br/><br/><br/>" );
+		HTML.push( "<div class=" + messages[i].type + "><p>" + messages[i].content + "</p></div>" );
 		$(HTML[i]).appendTo("#messageBox");
 	}
 }
@@ -14,7 +14,28 @@ function loadMessages () {
 
 function insertMessage(newMessage) {
 	var HTML = [];
-	HTML.push ( "<div class=outcommingMessage><p>" + newMessage + "</p></div><br/><br/><br/><br/><br/>" );
+	var today = new Date();
+	var dd = today.getDate();
+	var mm = today.getMonth()+1; //January is 0!
+	var yyyy = today.getFullYear();
+	var HH = today.getHours();
+	var MM = today.getMinutes();
+	if(dd<10) {
+	    dd='0'+dd
+	} 
+	if(mm<10) {
+	    mm='0'+mm
+	} 
+	if (HH<10) {
+		HH='0' + HH
+	}
+	if (MM<10) {
+		MM='0' + MM
+	}
+
+	today = HH + ':' + MM + ', '+ dd+'. '+ mm +'. '+yyyy;
+	
+	HTML.push ("<div class=outcommingMessage><p class=messageInfo><span class=sender>David Hasselhof</span><span class=messageDate>" + today + "</span></p><p>" + newMessage + "</p></div>");
 	$(HTML[0]).appendTo("#messageBox");
 
 }
